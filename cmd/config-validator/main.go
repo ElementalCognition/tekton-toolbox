515645fbdb533c16983939e68d9ea3bb78a1b414
Mike Serchenia <michael_serchenia@epam.com>
2023-07-26 13:19:09 -0500

    fix deprecations  config-testing

--- cmd/config-validator/main.go
+++ cmd/config-validator/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ElementalCognition/tekton-toolbox/pkg/pipelinerun"
 	"github.com/fatih/color"
 	"github.com/spf13/pflag"
-	"github.com/tektoncd/pipeline/pkg/apis/config"
+	cfgtesting "github.com/tektoncd/pipeline/pkg/apis/config/testing"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"sigs.k8s.io/yaml"
 )
@@ -89,9 +89,9 @@ func processPipelineRuns(cfg *pipelineconfig.Config) ([]byte, bool) {
 		}
 		pprYaml = append(pprYaml, pp...)
 
-		ctx := context.TODO()
+		ctx := context.Background()
 		if alphaFeatureGate {
-			ctx = config.EnableAlphaAPIFields(ctx)
+			ctx = cfgtesting.EnableAlphaAPIFields(ctx)
 		}
 
 		errs := p.Spec.Validate(ctx)
