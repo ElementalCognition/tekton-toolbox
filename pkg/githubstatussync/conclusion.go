19adf793a5824c87ba7dc7e6c96fbe44d77645db
Kirill Pchelintsev <kirillp@ec.ai>
2024-01-16 02:05:06 +0100

    OSL-492 Actually adding new file...

---
+++ pkg/githubstatussync/conclusion.go
@@ -0,0 +1,89 @@
+package githubstatussync
+
+import (
+	"context"
+	"strings"
+
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	"knative.dev/pkg/logging"
+)
+
+var optionalMarker = "optional-task"
+
+// Subset of check conclusions when check has "completed" status.
+// https://docs.github.com/en/rest/guides/using-the-rest-api-to-interact-with-checks?apiVersion=2022-11-28#about-check-suites
+const (
+	checkRunConclusionSuccess   = "success"
+	checkRunConclusionNeutral   = "neutral"
+	checkRunConclusionTimedOut  = "timed_out"
+	checkRunConclusionCancelled = "cancelled"
+	checkRunConclusionFailure   = "failure"
+)
+
+// Verify if the paramater list for a TaskRun contains this variable set to true (string).
+func hasOptionalMarker(trp v1beta1.Params) bool {
+	for _, p := range trp {
+		if p.Name == optionalMarker && p.Value.StringVal == "true" {
+			return true
+		}
+	}
+	return false
+}
+
+// Resolve specific reason for failure based on the TaskRunStatus via Conditions (depends on Reason and Message).
+func getFailureConclusion(ctx context.Context, trs v1beta1.TaskRunStatus) string {
+	var failureConclusion, reason, message string
+
+	logger := logging.FromContext(ctx)
+
+	trsLen := len(trs.GetConditions())
+	if trsLen == 0 {
+		logger.Errorw("Received empty conditions, can't determine status %v", trs)
+		return checkRunConclusionFailure
+	}
+	logger.Warnf("TaskRun status: %+v", trs.GetConditions())
+
+	// TODO Find how tekton orders conditions.
+	reason = trs.GetConditions()[trsLen-1].GetReason()
+	message = trs.GetConditions()[trsLen-1].GetMessage()
+	switch reason {
+	case v1beta1.TaskRunReasonCancelled.String():
+		// It seems to be pretty hard to actually get a clear TimedOut reason for a task.
+		// During tests I was unable to get it without PipelineRun time out cancelling it first.
+		// So we somewhat hack around this problem by checking the message and deciding based on it.
+		failureConclusion = checkRunConclusionCancelled
+		if strings.Contains(
+			message,
+			"TaskRun cancelled as the PipelineRun it belongs to has timed out.",
+		) {
+			logger.Warnf("Detected PipelineRun timeout, counting as general timeout")
+			failureConclusion = checkRunConclusionTimedOut
+		}
+	case v1beta1.TaskRunReasonTimedOut.String():
+		failureConclusion = checkRunConclusionTimedOut
+	default:
+		failureConclusion = checkRunConclusionFailure
+	}
+
+	logger.Warnf("Resolved conclusion: %s", failureConclusion)
+	return failureConclusion
+}
+
+// Resolve github conclusion for completed TaskRuns.
+func conclusion(ctx context.Context, status string, tr *v1beta1.TaskRun) string {
+	var conclusion string
+	logger := logging.FromContext(ctx)
+
+	if status == checkRunConclusionSuccess {
+		return checkRunConclusionSuccess
+	}
+
+	if hasOptionalMarker(tr.Spec.Params) {
+		logger.Warnf("Found optional marker: %+v", tr.Spec.Params)
+		conclusion = checkRunConclusionNeutral
+	} else {
+		conclusion = getFailureConclusion(ctx, tr.Status)
+	}
+
+	return conclusion
+}
